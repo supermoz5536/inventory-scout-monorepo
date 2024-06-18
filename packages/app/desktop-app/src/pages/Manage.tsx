8a1f6cfef64ac771aff3d02ccfc145c0e5d9e8dc
STF 5 <>
2024-06-19 04:35:47 +0900

    スクロール機能の実装 / コンテナーの配置
    ・リスト領域のx軸のスクロール機能を実装
    ・インプット領域の下にコンテナーを実装
    ・リスト領域の下にコンテナーを実装

--- packages/app/desktop-app/src/pages/Manage.tsx
+++ packages/app/desktop-app/src/pages/Manage.tsx
@@ -39,80 +39,87 @@ function Manage() {
     <div className="App">
       {/* タブ部分 */}
       <div className="square-space-tab">
-        <button className="square-space-tab-button">Aメイン画面</button>
+        <button className="square-space-tab-button">メイン画面</button>
         <button className="square-space-tab-button">ASIN管理</button>
       </div>
       <div className="body">
-        <input type="text" className="input-asin" />
+        <div className="left-column">
+          <input type="text" className="input-asin" />
+          <div className="left-column-down-container"></div>
+        </div>
 
-        {/* リスト全体 */}
-        <div className="globalList">
-          {/* リストヘッダー部分 */}
-          <div className="asin-list-header">
-            {/* 要素 ID */}
-            <div className="square-space-amazon-num">ID</div>
-            {/* 要素0 チェック */}
-            <div className="square-space-amazon-num">削除</div>
-            {/* 要素1 ASIN */}
-            <div className="square-space-asin">{<p>ASIN</p>}</div>
-
-            {/* 要素4 商品名 */}
-            <div className="square-space-name">
-              <p>商品名</p>
-            </div>
-
-            {/* 要素5 st-code */}
-            <div className="square-space-st-code">
-              <p>st_code</p>
-            </div>
-
-            {/* 要素6 lock-flag */}
-            <div className="square-space-lock-flag">
-              <p>lock-flag</p>
-            </div>
-
-            {/* 要素7 親ASIN */}
-            <div className="square-space-asin">
-              <p> 親ASIN</p>
-            </div>
-          </div>
-          {/* リスト部分 */}
-          {asinArray.map((asin, index) => (
-            <div className="asin-list">
+        <div className="right-column">
+          {/* リスト全体 */}
+          <div className="globalList">
+            {/* リストヘッダー部分 */}
+            <div className="asin-list-header">
               {/* 要素 ID */}
-              <div className="square-space-amazon-num">
-                <p>{index}</p>
-              </div>
+              <div className="square-space-amazon-num">ID</div>
               {/* 要素0 チェック */}
-              <div className="square-space-amazon-num">
-                <label>
-                  <input type="checkbox" />
-                </label>
-              </div>
+              <div className="square-space-amazon-num">削除</div>
               {/* 要素1 ASIN */}
-              <div className="square-space-asin">{<p>{asin}</p>}</div>
+              <div className="square-space-asin">{<p>ASIN</p>}</div>
 
               {/* 要素4 商品名 */}
-              <div className="square-space-name">{<p>{productName}</p>}</div>
+              <div className="square-space-name">
+                <p>商品名</p>
+              </div>
 
               {/* 要素5 st-code */}
               <div className="square-space-st-code">
-                <p>取得完了</p>
+                <p>st_code</p>
               </div>
 
               {/* 要素6 lock-flag */}
-              <div className="square-space-amazon-num">
-                <p>0</p>
+              <div className="square-space-lock-flag">
+                <p>lock-flag</p>
               </div>
 
               {/* 要素7 親ASIN */}
               <div className="square-space-asin">
-                <p>0</p>
+                <p> 親ASIN</p>
               </div>
             </div>
-          ))}
+            {/* リスト部分 */}
+            {asinArray.map((asin, index) => (
+              <div className="asin-list">
+                {/* 要素 ID */}
+                <div className="square-space-amazon-num">
+                  <p>{index}</p>
+                </div>
+                {/* 要素0 チェック */}
+                <div className="square-space-amazon-num">
+                  <label>
+                    <input type="checkbox" />
+                  </label>
+                </div>
+                {/* 要素1 ASIN */}
+                <div className="square-space-asin">{<p>{asin}</p>}</div>
+
+                {/* 要素4 商品名 */}
+                <div className="square-space-name">{<p>{productName}</p>}</div>
+
+                {/* 要素5 st-code */}
+                <div className="square-space-st-code">
+                  <p>取得完了</p>
+                </div>
+
+                {/* 要素6 lock-flag */}
+                <div className="square-space-amazon-num">
+                  <p>0</p>
+                </div>
+
+                {/* 要素7 親ASIN */}
+                <div className="square-space-asin">
+                  <p>0</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="right-column-down-container"></div>
         </div>
       </div>
+      <div className="body-bottom-container"></div>
     </div>
   );
 }
