a235f98d74efbfde0ab1a9d07e8fb2cd2d65a306
STF 5 <>
2024-06-20 23:54:38 +0900

    configureStore関数でのStoreの簡易的な枠組みの用意が完了

--- packages/app/desktop-app/src/redux/store.tsx
+++ packages/app/desktop-app/src/redux/store.tsx
@@ -1,3 +1,10 @@
-// store: 状態(=state)と状態変更関数(=Notifier)をまとめて、各sliceを格納したコンテナ
+import { configureStore } from "@reduxjs/toolkit";
+
+// store: 状態(=state)と、状態変更関数のreducer(=Notifier)をまとめて、各sliceを格納したコンテナ
 // このstoreというコンテナ内の情報を安全にグローバルに公開するのがProvider
 // ReduxのProviderの概念はFlutterのRiverpodと同じと考えていい。
+
+// configureStore: storeを簡単に作ることのできる関数
+const store = configureStore({
+  reducer: {},
+});
