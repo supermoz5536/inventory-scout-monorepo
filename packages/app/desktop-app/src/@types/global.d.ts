58dd53710df0d05e4cd4c00a3062f439d36a4805
STF 5 <>
2024-06-21 23:01:40 +0900

    ASINの追加機能実装完了
    ・グローバル状態管理変数 asinListに複数のASINをコピペで追加
    ・重複するASINを取り除く機能
    ・入力ASIN文字列を改行でスプリットする機能
    ・入力ASIN文字列を縦列に1ASINずつ表示する機能

--- packages/app/desktop-app/src/@types/global.d.ts
+++ packages/app/desktop-app/src/@types/global.d.ts
@@ -5,4 +5,18 @@ export declare global {
       openExternal: (url: string) => void | null;
     };
   }
+  // ASINData型のオブジェクトが
+  // string型のasinプロパティを
+  // 持つことを定義している。
+  interface AsinData {
+    asin: string;
+  }
+
+  // initialStateの型定義
+  // ASINState型のオブジェクトが
+  // ASINData型のvalueプロパティを
+  // 持つことを定義している。
+  interface AsinState {
+    value: AsinData[];
+  }
 }
