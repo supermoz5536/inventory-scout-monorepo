c2855cd0f351eb04039427012c4f0ea39509305f
STF 5 <>
2024-06-30 21:41:01 +0900

    App.tsxでマウント時に一回だけトリガーされる以下関数を用意
    ・マウント時にasinDataListにイテレート処理をかけて
    ・以下の要素を1つ以上見つけた場合にTrueを取得する
    　・（本日中にアプリ中断と、アプリの再起動が発生したと判断）
    　・（当てはまらない場合は、指定時刻の定期スクレイピングで対応）
    　・isScraping === true
    　・かつ
    　・fetchLatestDate === todayFormatted
    
    ・Trueの場合、runScrapingを起動する
    ・isScrapingのFormatはせずにrunScrapingの引数にそのまま渡す。

--- packages/app/desktop-app/src/App.tsx
+++ packages/app/desktop-app/src/App.tsx
@@ -2,14 +2,14 @@ import React, { useCallback, useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Top from "./pages/Top";
 import Manage from "./pages/Manage";
-import { AppDispatch } from "./redux/store";
-import { useDispatch } from "react-redux";
+import { AppDispatch, RootState } from "./redux/store";
+import { useDispatch, useSelector } from "react-redux";
 import { updateAsinData } from "./redux/asinDataListSlice";
 
 const App: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
 
-  // useCallbackを使用して関数の参照を安定させる
+  /// useCallbackを使用して関数の参照を安定させる
   const handleScrapingResult = useCallback(
     (event: Electron.IpcRendererEvent, data: AsinData) => {
       console.log("取得データ =", data);
@@ -18,9 +18,9 @@ const App: React.FC = () => {
     []
   );
 
-  // アプリ立ち上げの初期化処理として
-  // メインプロセスでのスクレイピング結果の取得する
-  // リスナーを配置します。
+  /// アプリ立ち上げの初期化処理として
+  ///  メインプロセスでのスクレイピング結果の取得する
+  ///  リスナーを配置します。
   useEffect(() => {
     console.log("scrapingResult called");
     window.myAPI.scrapingResult(handleScrapingResult);
@@ -30,6 +30,37 @@ const App: React.FC = () => {
       window.myAPI.removeScrapingResult(handleScrapingResult);
     };
   }, [handleScrapingResult]);
+
+  // グローバル変数のASINリストの値を取得
+  const asinDataList = useSelector(
+    (state: RootState) => state.asinDataList.value
+  );
+
+  /// アプリ終了によるスクレイピングの中断があった場合に
+  /// 中断したところから取得処理を再開する
+  /// 自動フォローアップ処理
+  /// マウント時に一回だけ起動
+  useEffect(() => {
+    if (asinDataList.length > 0) {
+      const today = new Date();
+      const todayFormatted = `${today.getFullYear()}-${String(
+        today.getMonth() + 1
+      ).padStart(2, "0")}-${String(today.getDate()).padStart(2, "0")}`;
+      const checkArray = asinDataList.find((asinData) => {
+        // 以下２点を満たすとTrue
+        // スクレイピングが取得中で
+        // 今日の日付のStockCountDataが存在してる場合
+        return (
+          asinData.isScraping === true &&
+          Object.keys(asinData.fbaSellerDatas).includes(todayFormatted)
+        );
+      });
+
+      if (checkArray) {
+        window.myAPI.runScraping(asinDataList);
+      }
+    }
+  }, []);
 
   return (
     <div>
