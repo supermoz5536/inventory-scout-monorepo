67c3fcf9c63681b245b08b49ba3aaa6183a51a10
STF 5 <>
2024-07-01 17:20:04 +0900

    electron.ts に saveDataとloadData関数を定義

--- packages/app/desktop-app/electron/electron.ts
+++ packages/app/desktop-app/electron/electron.ts
@@ -7,6 +7,7 @@
 import { app, BrowserWindow, ipcMain } from "electron";
 import * as path from "path";
 import * as url from "url";
+import * as fs from "fs";
 import scrapePromis from "../src/components/Scrape";
 
 const createWindow = () => {
@@ -41,6 +42,12 @@ const createWindow = () => {
   }
 };
 
+// ipcMain.handle メソッドは、メインプロセスで
+// 特定のIPC (Inter-Process Communication) チャンネルを設定し、
+// そのチャンネルに対する非同期のリクエストを
+// ハンドルするために使用されます。
+// これにより、レンダラープロセスからメインプロセスに対して
+// 非同期でメッセージを送信し、応答を受け取ることができます。
 ipcMain.handle("runScraping", async (event, asinDataList: AsinData[]) => {
   try {
     const scrape = await scrapePromis;
@@ -51,11 +58,83 @@ ipcMain.handle("runScraping", async (event, asinDataList: AsinData[]) => {
   }
 });
 
+ipcMain.handle("save-data", (event, data) => {
+  // ローカルストレージのpathを取得
+  const storagePath = path.join(__dirname, "scrapingData.json");
+  return new Promise<void>((resolve, reject) => {
+    // fs.writeFile: node.jsのファイル書き込みメソッド
+    // 構文: fs.writeFile(path, data, callback)
+    // JSON形式のデータは基本的にUTF-8エンコーディングで保存されます。
+    fs.writeFile(storagePath, JSON.stringify(data, null, 2), (err) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve();
+    });
+  });
+});
+
+ipcMain.handle("load-data", () => {
+  const storagePath = path.join(__dirname, "scrapingData.json");
+  return new Promise((resolve, reject) => {
+    // fs.readFile メソッドで "utf8" を指定すると
+    // ファイルの内容をUTF-8エンコーディングとして読み取ります。
+    // fs.readFile のコールバック関数の引数 (err, data) は、
+    // ファイル読み取り操作が完了したときに
+    // fs.readFile 関数によって自動的に提供されます。
+    fs.readFile(storagePath, "utf8", (err, data) => {
+      if (err) {
+        // エラーが発生した場合、
+        // 特にファイルが存在しない場合
+        // "Error NO ENTry" の略で、
+        // 指定されたファイルや
+        // ディレクトリが存在しない場合のエラー
+        if (err.code === "ENOENT") {
+          // ENOENTは、ファイルが存在しないことを示すエラー
+          // ファイルがまだ作成されていない場合に発生するため、
+          // 重大なエラーではないとみなし、
+          // 空の配列を返して通常の処理を続行する
+          return resolve([]);
+        }
+        // その他のエラーは、システムエラーの可能性があるため、
+        // rejectして、適切に処理できるようにする
+        return reject(err);
+      }
+      // JSON.parse は、
+      // 無効な JSON 文字列をパースしようとしたときに
+      // SyntaxError 例外をスローします。
+      try {
+        // ローカルに保存されている
+        // Jsonファイル(jsonString)を
+        // jsで扱えるように .parse で
+        // JavaScriptオブジェクトに変換します。
+        const parsedData = JSON.parse(data);
+        resolve(parsedData);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
+});
+
+// app.on メソッドは、
+// Electronのライフサイクルに関連するイベントリスナーを
+// 設定するために使用されます。
+// これにより、アプリケーションが特定のイベント
+// （例：起動、終了、ウィンドウが閉じられたときなど）
+// に対して特定のアクションを実行することができます。
 app.on("window-all-closed", () => {
+  // window-all-closed は、
+  // Electron の app オブジェクトのイベントの1つ。
+  // "darwin": macOS
+  // macOS以外のプラットフォームでは
+  // 窓を全部閉じたら、アプリケーションも終了させる
   if (process.platform !== "darwin") app.quit();
 });
 
 app.whenReady().then(() => {
+  // macOS では、ドックアイコンがクリックされてアプリがアクティブになったとき、
+  // 既存のウィンドウがない場合に新しいウィンドウを再作成する。
   createWindow();
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
