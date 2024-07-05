b9f33d80b5b45fd0f77f8b968882f972aa44f56f
STF 5 <>
2024-07-05 14:30:47 +0900

    Guestディレクトリと各コンポーネントファイルの作成

--- packages/app/desktop-app/src/pages/Setting.tsx
+++ packages/app/desktop-app/src/pages/Setting.tsx
@@ -1,49 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Setting.css";
 
 const Setting = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
+
+  // // onLogin関数の実装例
+  // const handleLogin = (email) => {
+  //   // 通常ここでAPIを呼び出して認証処理を行う
+  //   // 認証成功後にログイン状態を更新
+  //   setIsLoggedIn(true);
+  //   setUserEmail(email);
+  // };
+
+  // const handleLogout = () => {
+  //   setIsLoggedIn(false);
+  //   setUserEmail("");
+  // };
+
   return (
     <div>
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;{" "}
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;{" "}
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;{" "}
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;{" "}
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
-      <div className="setting-test">Setting</div>;
+      <h1>設定</h1>
+      {isLoggedIn ? (
+        <>
+          {/* ログイン設定のコンポーネント */}
+          {/* アカウント設定のコンポーネント */}
+          {/* 定期スクレイピング設定のコンポーネント */}
+        </>
+      ) : (
+        <>
+          {/* ログイン設定のコンポーネント */}
+          {/* アカウント設定のコンポーネント */}
+          {/* 定期スクレイピング設定のコンポーネント */}
+        </>
+      )}
     </div>
   );
 };
