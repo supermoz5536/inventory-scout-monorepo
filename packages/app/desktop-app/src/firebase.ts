320455b4f7d2dcc4ee092688b679d5ff48b8ffcf
STF 5 <>
2024-07-06 15:46:45 +0900

    firebaseの初期化用ファイルの作成
    ・src > firebase.ts: ファイルの作成

---
+++ packages/app/desktop-app/src/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDiTfLaMmthJxUrhPCsTI_eGgUQqmipQtU",
+  authDomain: "inventory-scout-ja.firebaseapp.com",
+  projectId: "inventory-scout-ja",
+  storageBucket: "inventory-scout-ja.appspot.com",
+  messagingSenderId: "671258679184",
+  appId: "1:671258679184:web:81d69be92d7445c5e84515",
+  measurementId: "G-LRZNYREKY9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
