fa1b9d24bda38be9584b1736687f59c22b87b6f9
STF 5 <>
2024-07-14 00:32:22 +0900

    ポップバーの表示位置を修正
              anchorOrigin={{
                vertical: "bottom",
                horizontal: "center",
              }}
              transformOrigin={{
                vertical: "top",
                horizontal: "center",
              }}
              // slotProps は MUI コンポーネントのスロット（内部要素）に
              // カスタムプロパティやスタイルを適用するために使用されます。
              slotProps={{
                // Paper 要素は カード、ポップオーバーなどの
                // コンテナに関わる設定に使用されます。
                paper: {
                  // sx要素で全体の場所を移動します。
                  sx: {
                    mt: 2,
                  },
                },
              }}

--- packages/app/desktop-app/src/components/stock-detail/Calender.tsx
+++ packages/app/desktop-app/src/components/stock-detail/Calender.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
-// import Calendar from "react-calendar";
-import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { LocalizationProvider, StaticDatePicker } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-
 import { TextField, Popover, Box, InputAdornment } from "@mui/material";
 import { subDays } from "date-fns";
+import jaLocale from "date-fns/locale/ja";
 import "react-calendar/dist/Calendar.css";
 import { CgCalendarToday } from "react-icons/cg";
 
@@ -57,79 +56,119 @@ const Calender = ({ onChange }: StockDetailProps) => {
   const open = Boolean(anchorEl);
 
   return (
-    <Box display="flex" alignItems="center" gap={2} className="custom-calendar">
-      <TextField
-        label="開始日"
-        // toLocaleDateString: Date型のobjを日本語の日付形式で表示します。
-        value={startDate.toLocaleDateString("ja-JP")}
-        onClick={(e) => handleOpen(e, "start")}
-        InputProps={{
-          readOnly: true,
-          sx: {
-            height: 35,
-            width: 116,
-            paddingTop: 0.5,
-          },
-          startAdornment: (
-            <InputAdornment
-              position="start"
-              sx={{
-                marginTop: -0.25,
-                marginLeft: -0.5,
-                marginRight: 0.5,
-              }}
-            >
-              <CgCalendarToday />
-            </InputAdornment>
-          ),
-        }}
-        sx={{ marginTop: 3, marginLeft: 115, marginBottom: 3 }}
-      />
-      <TextField
-        label="終了日"
-        value={endDate.toLocaleDateString("ja-JP")}
-        onClick={(e) => handleOpen(e, "end")}
-        InputProps={{
-          readOnly: true,
-          sx: {
-            height: 35,
-            width: 116,
-            paddingTop: 0.5,
-          },
-          startAdornment: (
-            <InputAdornment
-              position="start"
-              sx={{
-                marginTop: -0.25,
-                marginLeft: -0.5,
-                marginRight: 0.5,
-              }}
-            >
-              <CgCalendarToday />
-            </InputAdornment>
-          ),
-        }}
-        sx={{ marginTop: 3, marginBottom: 3 }}
-      />
-      <Popover
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "left",
-        }}
+    // LocalizationProvider は
+    // スコープないの全ての日付ピッカーコンポーネント (DatePicker等)に
+    // 設定を与えることで、一元管理の機能を果たします。
+
+    // dateAdapter プロパティは、
+    // 日付操作のためのライブラリを指定します。(date-fns等)
+
+    // AdapterDateFnsをpropsで渡す理由は
+    // LocalizationProviderで日付操作のためのライブラリを設定し
+    // 日付ピッカーコンポーネントで使えるようにするためです
+    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={jaLocale}>
+      <Box
+        display="flex"
+        alignItems="center"
+        gap={2}
+        className="custom-calendar"
       >
-        <Box p={2}>
-          <Calendar
-            // このプロパティは、ユーザーがカレンダーで日付を選択したときに
-            // 呼び出されるコールバック関数を指定します。
-            onChange={(date) => handleDateChange(date)}
-            value={currentPicker === "start" ? startDate : endDate}
-          />
-        </Box>
-      </Popover>
-    </Box>
+        <TextField
+          label="開始日"
+          // toLocaleDateString: Date型のobjを日本語の日付形式で表示します。
+          value={startDate.toLocaleDateString("ja-JP")}
+          onClick={(e) => handleOpen(e, "start")}
+          InputProps={{
+            readOnly: true,
+            sx: {
+              height: 35,
+              width: 116,
+              paddingTop: 0.5,
+            },
+            startAdornment: (
+              <InputAdornment
+                position="start"
+                sx={{
+                  marginTop: -0.25,
+                  marginLeft: -0.5,
+                  marginRight: 0.5,
+                }}
+              >
+                <CgCalendarToday />
+              </InputAdornment>
+            ),
+          }}
+          sx={{ marginTop: 3, marginLeft: 115, marginBottom: 3 }}
+        />
+        <TextField
+          label="終了日"
+          value={endDate.toLocaleDateString("ja-JP")}
+          onClick={(e) => handleOpen(e, "end")}
+          InputProps={{
+            readOnly: true,
+            sx: {
+              height: 35,
+              width: 116,
+              paddingTop: 0.5,
+            },
+            startAdornment: (
+              <InputAdornment
+                position="start"
+                sx={{
+                  marginTop: -0.25,
+                  marginLeft: -0.5,
+                  marginRight: 0.5,
+                }}
+              >
+                <CgCalendarToday />
+              </InputAdornment>
+            ),
+          }}
+          sx={{ marginTop: 3, marginBottom: 3 }}
+        />
+        <Popover
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center",
+          }}
+          // slotProps は MUI コンポーネントのスロット（内部要素）に
+          // カスタムプロパティやスタイルを適用するために使用されます。
+          slotProps={{
+            // Paper 要素は カード、ポップオーバーなどの
+            // コンテナに関わる設定に使用されます。
+            paper: {
+              // sx要素で全体の場所を移動します。
+              sx: {
+                mt: 2,
+              },
+            },
+          }}
+        >
+          <Box p={2}>
+            {currentPicker === "start" ? (
+              <StaticDatePicker
+                displayStaticWrapperAs="desktop"
+                value={startDate}
+                onChange={handleDateChange}
+              />
+            ) : (
+              <StaticDatePicker
+                displayStaticWrapperAs="desktop"
+                value={endDate}
+                onChange={handleDateChange}
+              />
+            )}
+          </Box>
+        </Popover>
+      </Box>
+    </LocalizationProvider>
   );
 };
 
