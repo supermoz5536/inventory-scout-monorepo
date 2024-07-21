a8f4f72b3e4b90243fae4e06ebd5278a17cfbd6e
STF 5 <>
2024-07-21 12:04:18 +0900

    make comppnent : calculateDecreaseTodayToYesterday

---
+++ packages/app/desktop-app/src/util/calculateDecreaseTodayToYesterday.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const calculateDecreaseTodayToYesterday = () => {
+  return <div>calculateDecreaseTodayToYesterday</div>;
+};
+
+export default calculateDecreaseTodayToYesterday;
