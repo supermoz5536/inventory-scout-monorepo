d0edfe54248c2a5ea7452f218aed0a4cfa5cee95
STF 5 <>
2024-08-14 14:46:23 +0900

    CreateAccountFormコンポーネントのバリデーション機能を実装完了

--- packages/app/desktop-app/src/components/account/CreateAccountForm.tsx
+++ packages/app/desktop-app/src/components/account/CreateAccountForm.tsx
@@ -8,6 +8,7 @@ import {
   DialogTitle,
   Link,
   TextField,
+  Typography,
 } from "@mui/material";
 import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -15,6 +16,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 const CreateAccountForm = ({
   isOpenCreateAccountFormDialog,
@@ -30,51 +33,127 @@ const CreateAccountForm = ({
   };
   const handleClose = () => {
     setIsOpenCreateAccountFormDialog(false);
+    reset({ email: "", password: "", confirmPassword: "" }); // フォームの入力値とエラーステートをリセット
   };
+
+  // フォーム送信時の処理
+  const onSubmit: SubmitHandler<CreateAccountFormInput> = (data) => {
+    // バリデーションチェックOK！なときに行う処理を追加
+    setIsOpenCreateAccountFormDialog(false);
+  };
+
+  // バリデーションルール
+  const schema = yup.object({
+    email: yup
+      .string()
+      .required("メールアドレスを入力してください。")
+      .email("メールアドレスの形式が正しくありません。"),
+    password: yup
+      .string()
+      .required("パスワードを入力してください。")
+      .min(6, "パスワードは６文字以上が必要です。")
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&].*$/,
+        "パスワードは、英字、数字、および特殊文字をそれぞれ1文字以上含む必要があります。"
+      ),
+    confirmPassword: yup
+      .string()
+      .required("パスワードの確認")
+      // oneOf メソッドは、値が指定された配列の中のいずれかと
+      // 一致することを確認するためのバリデーションルールです。
+      .oneOf([yup.ref("password")], "パスワードが一致しません。"),
+  });
+
+  // フォーム管理補助ライブラリの React Hook Form から、以下を分割代入
+  // ・各フォームを接続する register
+  // ・登録情報の送信時のコールバックを設定する handleSubmit
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<CreateAccountFormInput>({
+    resolver: yupResolver(schema),
+  });
+
   return (
     <React.Fragment>
       <Dialog open={isOpenCreateAccountFormDialog} onClose={handleClose}>
         <DialogTitle
           sx={{
-            marginLeft: "35px",
-            marginBottom: "0px",
             fontSize: "25px",
             fontWeight: "bold",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
           }}
         >
-          アカウントの作成
+          アカウント作成
         </DialogTitle>
         <DialogContentText>
-          <p>永久プランの購入にはアカウントが必要です。</p>
+          <Typography
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+            }}
+          >
+            プランのご購入にはアカウントが必要です。
+          </Typography>
         </DialogContentText>
         <DialogContent>
           <TextField
             autoFocus
             required
             margin="dense"
-            id="name"
-            name="email"
+            id="email"
             label="メールアドレス"
             type="email"
             fullWidth
             variant="standard"
-            // value={inputEmail}
-            // onChange={handleInputEmailChange}
+            {...register("email")}
+            error={"email" in errors} // エラーが含まれているかを確認するプロパティ
+            helperText={errors.email?.message} // エラー内容をUIに表示
           />
           <TextField
             autoFocus
             required
             margin="dense"
-            id="name"
-            name="email"
+            id="password"
             label="パスワード"
             // type={isVisiblePassword ? "text" : "password"}
             fullWidth
             variant="standard"
-            // value={inputPassword}
-            // onChange={handleInputPasswordChange}
+            {...register("password")}
+            error={"password" in errors}
+            helperText={errors.password?.message}
+          />
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="confirmPassword"
+            label="パスワードの確認"
+            // type={isVisiblePassword ? "text" : "password"}
+            fullWidth
+            variant="standard"
+            {...register("confirmPassword")}
+            error={"confirmPassword" in errors}
+            helperText={errors.confirmPassword?.message}
           />
         </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>キャンセル</Button>
+          <Button
+            variant="contained"
+            onClick={handleSubmit(onSubmit)}
+            sx={{
+              fontWeight: "bold",
+            }}
+          >
+            作成
+          </Button>
+        </DialogActions>
       </Dialog>
     </React.Fragment>
   );
